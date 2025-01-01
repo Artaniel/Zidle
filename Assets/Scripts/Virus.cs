a2123d9479a1b22fcb9517decdd20929588fcc14
Artaniel <59449523+Artaniel@users.noreply.github.com>
2025-01-01 23:49:45 +0300

    rpg stat experiments

--- Assets/Scripts/Virus.cs
+++ Assets/Scripts/Virus.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Virus : ManualMonobehaviour
 {
     public float incubationSpeed = 0.01f;
+    private float defaultIncubationSpeed;
     public List<EvolutionPerk> activePerks;
 
-    public override void Init() { 
-        
+    public override void Init() {
+        defaultIncubationSpeed = incubationSpeed;
     }
 
     public override void ManualFixedUpdate() {
+        
+    }
+
+    public void RefreshStatsFromPerks() { 
         
     }
 }
