541818a0be05eaf1030107483bbbd9679a8b2d10
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-02-07 22:47:09 +0300

    EvolutionPerk abstraction

--- Assets/Scripts/EvolutionPerks/EvolutionPerk.cs
+++ Assets/Scripts/EvolutionPerks/EvolutionPerk.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public abstract class EvolutionPerk : ManualMonobehaviour
 {
-
+    public string displayName = "placeholder";
+    public string description = "placeholder";
+    public Sprite icon;
+    public int cost;
+    public int maxLevel;
+    public int currentLevel;
+    
 }
